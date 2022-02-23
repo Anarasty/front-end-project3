5faba6e220256f0ed7a091f9f5c3f1d11c16b9e3
Anarasty <murloc.ninja16@gmail.com>
2022-02-23 20:31:38 +0200

    js output

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("demo2").innerHTML = 4 + 4;
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function myFunction2() {
+    window.alert("После того, как вы нажмете ОК, вас выебут в рот. Вы уверены в своем выборе?");
+    window.alert("точно?");
+}
+console.log("testing console");
+function myFunction3() {
+    window.print("print me");
+}
