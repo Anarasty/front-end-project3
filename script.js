6e42ca7044b5450faa77b9350ef62ea958698653
Anarasty <murloc.ninja16@gmail.com>
2022-03-20 15:57:35 +0200

    rewrited start

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.getElementById("demo2").innerHTML = 4 + 4;
+
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
+  document.getElementById("demo").style.backgroundColor='red';
+  document.getElementById("demo").style.fontSize='40px';
 }
+
 function myFunction2() {
     window.alert("После того, как вы нажмете ОК, вас выебут в рот. Вы уверены в своем выборе?");
     window.alert("точно?");
 }
+
 console.log("testing console");
 function myFunction3() {
     window.print("print me");
