5c3f3d1aa8b5847420123aa3be42c682a9dfe6e0
Anarasty <murloc.ninja16@gmail.com>
2022-03-21 10:20:13 +0200

    js syntax

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ console.log("testing console");
 function myFunction3() {
     window.print("print me");
 }
+
+// ---
+let a, b, c;
+a = 2;
+b = 3;
+c = a + b;
+document.getElementById("statements").innerHTML = "value of a + b is " + c;
+// ---
+
