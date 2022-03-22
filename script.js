1e724f89c09e5af10d6e04038fbb17563b06cc5f
Anarasty <murloc.ninja16@gmail.com>
2022-03-22 12:47:42 +0200

    JS let

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ document.getElementById("demo2").innerHTML = 4 + 4;
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
-  document.getElementById("demo").style.backgroundColor='red';
-  document.getElementById("demo").style.fontSize='40px';
+  document.getElementById("demo").style.backgroundColor = "red";
+  document.getElementById("demo").style.fontSize = "40px";
 }
 
 function myFunction2() {
-    window.alert("После того, как вы нажмете ОК, вас выебут в рот. Вы уверены в своем выборе?");
-    window.alert("точно?");
+  window.alert(
+    "После того, как вы нажмете ОК, вас выебут в рот. Вы уверены в своем выборе?"
+  );
+  window.alert("точно?");
 }
 
 console.log("testing console");
 function myFunction3() {
-    window.print("print me");
+  window.print("print me");
 }
 
 // ---
@@ -24,3 +26,50 @@ c = a + b;
 document.getElementById("statements").innerHTML = "value of a + b is " + c;
 // ---
 
+function myFunction4() {
+  let animalFirst = "cat";
+  document.getElementById("catDemo").innerHTML = animalFirst;
+}
+
+function myFunction5() {
+  let animalSecond;
+  document.getElementById("dogDemo").innerHTML = animalSecond;
+}
+
+function myFunction6() {
+  var myNumber = "One";
+  var myNumber;
+  document.getElementById("demoNumberVar").innerHTML = myNumber;
+}
+
+// ---
+let q = 5 + 2 + 3;
+let w = "John" + " " + "Doe";
+let e = "5" + 2 + 3;
+let r = 2 + 3 + "5";
+document.getElementById("demo3").innerHTML = q;
+document.getElementById("demo4").innerHTML = w;
+document.getElementById("demo5").innerHTML = e;
+document.getElementById("demo6").innerHTML = r;
+// ---
+
+{
+  // let CAN NOT BE USE IN BLOCK SCOPE {}
+  var monkey = "monkey";
+}
+document.getElementById("demo7").innerHTML = monkey;
+
+var creature = 'elf';
+{
+  var creature = "gnome";
+}
+
+console.log(creature);
+
+// ---
+let animal = 'cat';
+{
+  let animal = 'dog';
+}
+
+console.log('the animal is ' + animal);
